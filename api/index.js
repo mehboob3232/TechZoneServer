2e4045bfd679dcfe7af59d4c6f1aed44b0ac38b6
Mehboob_Ahmad <mehboob.ldn@gmail.com>
2023-08-10 16:25:34 +0500

    fffff

--- api/index.js
+++ api/index.js
@@ -1,41 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MoviesModels = require("../models/MoviesModels");
-const TvShowModel = require("../models/TvShowModel");
 const cors = require("cors");
 // const multer = require("multer");
 // const upload = multer({ dest: "../public/" });
-const  {v2} =require('cloudinary');
-       
-app.use(express.json());     
-v2.config({ 
-  cloud_name: 'dxyayoqgq', 
-  api_key: '436438823682386', 
-  api_secret: 'VIVrthzaiQJ2QFpYQZcjgKRQhFU' 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 app.get("/api", async (request, response) => {
-
-  v2.uploader.upload("http://localhost:3001//public/01d523516aa4134653c80127506b7d4b.jpeg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
 
     return response.json({
         status: true,
@@ -52,10 +24,6 @@ app.get("/api", async (request, response) => {
 
 mongoose.connect("mongodb+srv://mehboob05:XA78CAnYR35WsgSc@jobsite.2znsayl.mongodb.net/jobDb").then(() => {
    console.log("DB connect")
-
-   app.listen(3004, () => {
-    console.log(`BACKEND WORKING GOOD`);
-  });
 
   });
 
